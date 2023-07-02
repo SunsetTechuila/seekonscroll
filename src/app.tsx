ada8f44293b275262b98660e119a4173c19bc113
Xyntho <krishnab0406@gmail.com>
2023-07-02 15:37:10 +0530

    Wait for progBar to load

--- src/app.tsx
+++ src/app.tsx
@@ -1,37 +1,42 @@
 async function main() {
-  const progBar = document.querySelector<HTMLDivElement>(
-    "#main > div > div.Root__top-container > div.Root__now-playing-bar > footer > div > div.main-nowPlayingBar-center > div > div.playback-bar > div.playback-progressbar.playback-progressbar-isInteractive > div"
-  );
-  console.log("Seek on scroll loaded!")
+  
+  console.log("Seek on scroll loaded!");
   var checkProgBar = setInterval(() => {
-    if (progBar) {
+    if (document.querySelector<HTMLDivElement>(
+      "#main > div > div.Root__top-container > div.Root__now-playing-bar > footer > div > div.main-nowPlayingBar-center > div > div.playback-bar > div.playback-progressbar.playback-progressbar-isInteractive > div"
+    )) {
       onProgBarLoaded();
+      console.log("Prog bar loaded!");
       clearInterval(checkProgBar);
     }
   }, 10);
   const sensitivity = 0.2;
   let toPlay = false;
   function onProgBarLoaded() {
-      progBar!.addEventListener("wheel", (e) => {
+    const progBar = document.querySelector<HTMLDivElement>(
+      "#main > div > div.Root__top-container > div.Root__now-playing-bar > footer > div > div.main-nowPlayingBar-center > div > div.playback-bar > div.playback-progressbar.playback-progressbar-isInteractive > div"
+    );
+    if (progBar) {
+      progBar.addEventListener("wheel", (e) => {
         if (Spicetify.Player.isPlaying()) {
           Spicetify.Player.pause();
           toPlay = true;
         }
 
         let currentState = parseFloat(
-          progBar!.style.cssText.split(":")[1].split("%")[0]
+          progBar.style.cssText.split(":")[1].split("%")[0]
         );
 
         if (currentState <= 100 && currentState >= 0) {
           let changedVal = currentState + e.deltaY * sensitivity;
           if (e.deltaY > 0) {
             if (changedVal <= 100) {
-              progBar!.style.cssText =
+              progBar.style.cssText =
                 "--progress-bar-transform: " + changedVal + "%;";
             }
           } else {
             if (changedVal >= 0) {
-              progBar!.style.cssText =
+              progBar.style.cssText =
                 "--progress-bar-transform: " + changedVal + "%;";
             }
           }
@@ -39,6 +44,7 @@ async function main() {
         debounce(handleStyleChange, 300);
       });
 
+      // Create a new MutationObserver
       const observer = new MutationObserver((mutationsList) => {
         let isWheeling = false;
         // Check each mutation that occurred
@@ -54,18 +60,21 @@ async function main() {
           handleStyleChange();
         }
       });
-      observer.observe(progBar, { attributes: true });
+
+      // Select the target element
+      const targetElement = progBar;
+
+      // Start observing the target element for mutations
+      observer.observe(targetElement, { attributes: true });
     }
+
+    // Debounce function to delay the execution of the handler
     let debounceTimeout: ReturnType<typeof setTimeout>;
     function debounce(func: () => void, delay: number) {
       clearTimeout(debounceTimeout);
       debounceTimeout = setTimeout(func, delay);
     }
     function handleStyleChange() {
-      if (!Spicetify.Player.isPlaying() && toPlay) {
-        Spicetify.Player.play();
-        toPlay = false;
-      }
       if (progBar) {
         let currentState = parseFloat(
           progBar.style.cssText.split(":")[1].split("%")[0]
@@ -73,8 +82,13 @@ async function main() {
         let prog = currentState / 100;
         let newProg = Spicetify.Player.getDuration() * prog;
         Spicetify.Player.seek(newProg);
+        if (!Spicetify.Player.isPlaying() && toPlay) {
+          Spicetify.Player.play();
+          toPlay = false;
+        }
       }
     }
+  }
 }
 
 export default main;
